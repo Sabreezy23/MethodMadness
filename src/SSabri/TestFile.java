2140c7539fb2631fb70d4eef9641356e00a9b41a
Sabreezy <officialsabrun@gmail.com>
2018-09-24 20:26:19 -0400

    Update TestFile.java

--- src/SSabri/TestFile.java
+++ src/SSabri/TestFile.java
@@ -1,9 +1,23 @@
 //Sabrun Sabri 09/20/18//
 package SSabri;
-import static SSabri.SSabriLib.isPalindrome;
+
+import static SSabri.SSabriLib.*;
+
 public class TestFile {
     public static void main(String[] arge) {
-        boolean x = isPalindrome("lol");
-        System.out.print(x);
+        boolean a = isPalindrome("lol");
+        System.out.println(a);
+
+        String b = dateStr("12/10/2001");
+        System.out.println(b);
+
+        String c = cutOut("byehellohi","ehell");
+        System.out.println(c);
+
+        multiplicationTable(10,6);
+
+        int d = sumUpTo(100);
+        System.out.println(d);
+
     }
 }
